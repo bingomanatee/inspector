68bcfb82064541084864267202232d16ee669c10
davidedelhart <dave@wonderlandlabs.com>
2018-11-18 11:53:37 -0800

    established 1.0.4 version for npm

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,3 +2,8 @@
 
 refinement of nesting test functions; further polishing of and/or collector processes.
 Created andFn/orFn shorthands for boolean collectors. 
+
+## 1.0.4 
+
+bottled resources; allowed for custom functions, instantiation of closured-context 
+production of inspector methods
--- src/index.js
+++ src/index.js
@@ -4,5 +4,19 @@ const {
   ifFn, collector, validator, andFn, orFn, defineCustomFunction,
 } = bottle().container;
 
+const factory = (modifier) => {
+  const myBottle = bottle();
+  if (modifier) modifier(myBottle);
+  return (myBottle().container);
+};
+
 export default validator;
-export { ifFn, collector, validator, andFn, orFn, defineCustomFunction };
+export {
+  ifFn,
+  collector,
+  validator,
+  andFn,
+  orFn,
+  defineCustomFunction,
+  factory,
+};
